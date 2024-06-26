c7b52b6273ded1ab87f47acfe291a64fd585775c
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 22:41:44 -0400

    Add signin button for small screens

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,18 @@
+import SignInButton from "@/components/sigin-button";
+
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <div className="grow">
+      <p>hi</p>
+
+      {/* Signin button showing up at the bottom for small screens */}
+      <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:hidden">
+        <SignInButton
+          className={
+            "w-full rounded-full border border-blue-800 bg-blue-800 p-2 text-white shadow shadow-blue-200 hover:shadow-sm active:bg-white active:text-blue-800"
+          }
+        />
+      </div>
+    </div>
+  );
 }
