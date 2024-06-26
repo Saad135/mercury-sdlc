1b05dd11e803f4e479af27df788ea5bdcda1b496
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 22:20:27 -0400

    Make header layout for small screens

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -5,13 +5,13 @@ export default function HomeHeader() {
   return (
     <>
       <div className="w-full bg-white">
-        <div className="container mx-auto mb-4 flex items-center justify-between p-4">
+        <div className="container mx-auto mb-4 flex items-center justify-center p-4 sm:justify-between">
           <HomeLogo />
 
           {/* Sign In button */}
           <SignInButton
             className={
-              "rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white"
+              "hidden rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white sm:block"
             }
           />
         </div>
