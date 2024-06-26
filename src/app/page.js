8446adce9d1b38446492ba1ace54f28f3775691a
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 23:57:17 -0400

    Add bottom padding to home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import SignInButton from "@/components/sigin-button";
 
 export default function Home() {
   return (
-    <div className="grow overflow-auto">
+    <div className="grow overflow-auto pb-20">
       {/* List of cards */}
       <AttractionList listName={"Attractions"} />
 
