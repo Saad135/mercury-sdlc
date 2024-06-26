24610aa7137689b4f67bc4628557407f190bd399
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 22:04:42 -0400

    Move signing button to own component

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import SignInButton from "./sigin-button";
+
 export default function HomeHeader() {
   return (
     <>
@@ -10,11 +12,7 @@ export default function HomeHeader() {
           </Link>
 
           {/* Sign In button */}
-          <div>
-            <button className="rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white">
-              Sign in
-            </button>
-          </div>
+          <SignInButton />
         </div>
       </div>
     </>
---
+++ src/components/sigin-button.js
@@ -0,0 +1,11 @@
+export default function SignInButton() {
+  return (
+    <>
+      <div>
+        <button className="rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white">
+          Sign in
+        </button>
+      </div>
+    </>
+  );
+}
