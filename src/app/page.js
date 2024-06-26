48f9554101e454121bdb0f5f9d8baabff53b7023
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 22:52:22 -0400

    Start list of attractions section on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,12 @@ import SignInButton from "@/components/sigin-button";
 export default function Home() {
   return (
     <div className="grow">
-      <p>hi</p>
+      {/* List of cards */}
+      <div className="my-2 rounded-xl bg-white p-4 shadow sm:container sm:mx-auto">
+        <p className="mb-2">Attractions</p>
+
+        {/* Cards */}
+      </div>
 
       {/* Signin button showing up at the bottom for small screens */}
       <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:hidden">
