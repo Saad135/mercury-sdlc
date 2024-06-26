4de1a36d301d03a2927ea1449386942da15fe6e1
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 23:44:54 -0400

    Add shadow to cards

--- src/components/attraction-card.js
+++ src/components/attraction-card.js
@@ -4,7 +4,7 @@ export default function AttractionCard({ mediaUrl }) {
   return (
     <>
       {/* Card */}
-      <div className="mx-2 mb-4 mt-2 border border-blue-900 text-center sm:w-1/3 lg:w-1/4">
+      <div className="mx-2 mb-4 mt-2 border border-blue-900 text-center shadow-lg sm:w-1/3 lg:w-1/4">
         {/* Card Image */}
         <div className="relative h-72 w-full">
           <Image
