b286bd08f701c420b3fc975120e97bf966265c3a
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 23:14:42 -0400

    Refactor to attraction card component

---
+++ src/components/attraction-card.js
@@ -0,0 +1,16 @@
+export default function AttractionCard({}) {
+  return (
+    <>
+      <div className="w-full border border-green-900 sm:flex sm:flex-wrap">
+        {/* Card */}
+        <div className="mb-4 mt-2 border border-blue-900 text-center sm:w-1/3">
+          {/* Card Image */}
+          <div className="h-72 w-full"></div>
+
+          {/* Card title */}
+          <p>Title</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/attraction-list.js
+++ src/components/attraction-list.js
@@ -1,3 +1,5 @@
+import AttractionCard from "./attraction-card";
+
 export default function AttractionList({ listName }) {
   return (
     <>
@@ -5,16 +7,7 @@ export default function AttractionList({ listName }) {
         <p className="mb-2">{listName}</p>
 
         {/* Card list */}
-        <div className="w-full border border-green-900 sm:flex sm:flex-wrap">
-          {/* Card */}
-          <div className="mb-4 mt-2 border border-blue-900 text-center sm:w-1/3">
-            {/* Card Image */}
-            <div className="h-72 w-full"></div>
-
-            {/* Card title */}
-            <p>Title</p>
-          </div>
-        </div>
+        <AttractionCard />
       </div>
     </>
   );
