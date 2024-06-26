5f11c553607d8ec98e8fc7538a651609f0a13a37
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 21:56:55 -0400

    Style header logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +15,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-gray-100`}>
         {/* Header */}
-        <div className="container mx-auto mb-4 flex items-center justify-between bg-white p-4">
-          <p>Mercury</p>
+        <div className="w-full bg-white">
+          <div className="container mx-auto mb-4 flex items-center justify-between p-4">
+            <Link href="/">
+              <p className="text-3xl font-semibold text-blue-800">Mercury</p>
+            </Link>
 
-          {/* Sign In button */}
-          <div>
-            <button className="rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white">
-              Sign in
-            </button>
+            {/* Sign In button */}
+            <div>
+              <button className="rounded-full border border-blue-800 p-2 text-blue-800 shadow shadow-blue-200 hover:shadow-sm active:bg-blue-800 active:text-white">
+                Sign in
+              </button>
+            </div>
           </div>
         </div>
 
