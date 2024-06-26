cedbbd315f75d02f540f424cf6de5e33e4f62b98
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 23:12:13 -0400

    Define wip attraction list

--- src/components/attraction-list.js
+++ src/components/attraction-list.js
@@ -4,7 +4,17 @@ export default function AttractionList({ listName }) {
       <div className="my-2 rounded-xl bg-white p-4 shadow sm:container sm:mx-auto">
         <p className="mb-2">{listName}</p>
 
-        {/* Cards */}
+        {/* Card list */}
+        <div className="w-full border border-green-900 sm:flex sm:flex-wrap">
+          {/* Card */}
+          <div className="mb-4 mt-2 border border-blue-900 text-center sm:w-1/3">
+            {/* Card Image */}
+            <div className="h-72 w-full"></div>
+
+            {/* Card title */}
+            <p>Title</p>
+          </div>
+        </div>
       </div>
     </>
   );
