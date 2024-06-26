023e390e901e2a5cb05c6c52bcb25f8f560c6cb6
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-06-25 23:43:32 -0400

    Add support for unsplash images

--- src/components/attraction-card.js
+++ src/components/attraction-card.js
@@ -1,10 +1,25 @@
-export default function AttractionCard({}) {
+import Image from "next/image";
+
+export default function AttractionCard({ mediaUrl }) {
   return (
     <>
       {/* Card */}
       <div className="mx-2 mb-4 mt-2 border border-blue-900 text-center sm:w-1/3 lg:w-1/4">
         {/* Card Image */}
-        <div className="h-72 w-full"></div>
+        <div className="relative h-72 w-full">
+          <Image
+            className="size-full object-cover"
+            src={
+              mediaUrl
+                ? mediaUrl
+                : "https://images.unsplash.com/photo-1501256504904-1fbe305bb538"
+            }
+            alt="photo"
+            style={{ objectFit: "cover" }}
+            sizes="(max-width: 768px) 50vw, 20vw"
+            fill
+          />
+        </div>
 
         {/* Card title */}
         <p>Title</p>
