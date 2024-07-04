552953b40d246c5c2237b51b761227ed4ef026a4
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-04 11:28:27 -0400

    Make the sign in buttons the same button

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -8,22 +8,16 @@ export default function HomeHeader() {
         <div className="container mx-auto mb-4 flex items-center justify-center p-4 sm:justify-between">
           <HomeLogo />
 
-          {/* Sign In button */}
-          <SignInButton
-            className={
-              "hidden rounded-full border border-primary p-2 text-primary shadow shadow-primary-lighter hover:shadow-sm active:bg-primary active:text-white sm:block"
-            }
-          />
-        </div>
-
-        {/* Sign in button for smaller screens */}
-        {/* Signin button showing up at the bottom for small screens */}
-        <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:hidden">
-          <SignInButton
-            className={
-              "w-full rounded-full border border-primary bg-primary p-2 text-white shadow shadow-primary-lighter hover:shadow-sm active:bg-white active:text-primary"
-            }
-          />
+          {/* Sign in button for smaller screens */}
+          {/* Signin button showing up at the bottom for small screens */}
+          <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:relative sm:z-0 sm:my-0 sm:px-4">
+            {/* Sign In button */}
+            <SignInButton
+              className={
+                "w-full rounded-full border border-primary bg-primary p-2 text-white shadow shadow-primary-lighter hover:shadow-sm active:bg-white active:text-primary sm:bg-white sm:text-primary sm:active:bg-primary sm:active:text-white"
+              }
+            />
+          </div>
         </div>
       </div>
     </>
