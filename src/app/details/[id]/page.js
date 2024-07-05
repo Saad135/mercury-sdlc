bfb0b2feb2100290a4f74ea2fe7ed27ae70a6e8c
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-05 02:15:33 -0400

    Set Description

--- src/app/details/[id]/page.js
+++ src/app/details/[id]/page.js
@@ -3,21 +3,33 @@ import Container from "@/components/container";
 import { useDetailsPageStates } from "@/components/details/details-provider";
 import EventImage from "@/components/events/event-image";
 
-export default function DetailsPage({ params }) {
+export default function DetailsPage() {
   return (
     <>
       <Container>
         <HeroImage />
 
-        <MainTitle />
+        <EventTitle />
 
-        <div className="my-4">Hi {params?.id}</div>
+        <EventDescription />
       </Container>
     </>
   );
 }
 
-function MainTitle() {
+function EventDescription() {
+  const context = useDetailsPageStates();
+  return (
+    <>
+      <div
+        className="my-4"
+        dangerouslySetInnerHTML={{ __html: context?.event?.description }}
+      />
+    </>
+  );
+}
+
+function EventTitle() {
   const context = useDetailsPageStates();
 
   return (
