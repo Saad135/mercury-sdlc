054472e551146185187c9fa9b51a3f55cbf26aa1
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-04 21:30:44 -0400

    Refactor header sign-in button

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -6,20 +6,35 @@ export default function HomeHeader() {
     <>
       <div className="w-full bg-white">
         <div className="container mx-auto mb-4 flex items-center justify-center p-4 sm:justify-between">
-          <HomeLogo />
+          <HeaderLogo />
 
-          {/* Sign in button for smaller screens */}
-          {/* Signin button showing up at the bottom for small screens */}
-          <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:relative sm:z-0 sm:my-0 sm:px-4">
-            {/* Sign In button */}
-            <SignInButton
-              className={
-                "w-full rounded-full border border-primary bg-primary p-2 text-white shadow shadow-primary-lighter hover:shadow-sm active:bg-white active:text-primary sm:bg-white sm:text-primary sm:active:bg-primary sm:active:text-white"
-              }
-            />
-          </div>
+          <HeaderSignIn />
         </div>
       </div>
+    </>
+  );
+}
+
+function HeaderSignIn() {
+  return (
+    <>
+      {/* Signin button showing up at the bottom for small screens */}
+      <div className="absolute inset-x-0 bottom-0 z-50 my-10 px-4 sm:relative sm:z-0 sm:my-0 sm:px-4">
+        {/* Sign In button */}
+        <SignInButton
+          className={
+            "w-full rounded-full border border-primary bg-primary p-2 text-white shadow shadow-primary-lighter hover:shadow-sm active:bg-white active:text-primary sm:bg-white sm:text-primary sm:active:bg-primary sm:active:text-white"
+          }
+        />
+      </div>
+    </>
+  );
+}
+
+function HeaderLogo() {
+  return (
+    <>
+      <HomeLogo />
     </>
   );
 }
