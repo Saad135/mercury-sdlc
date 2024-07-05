879309ec04c24eb36bb8d3998312b0119af42f09
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-04 22:15:21 -0400

    Refactor attraction list

--- src/components/attraction-list.js
+++ src/components/attraction-list.js
@@ -5,26 +5,34 @@ import Container from "./container";
 import { useHomeStates } from "./home-provider";
 
 export default function AttractionList({ listName }) {
-  // States
-  const statesArr = useAttractionListStates();
-
   return (
     <>
       <AttractionContainer>
         <p className="mb-2 text-xl">{listName}</p>
 
-        {/* Card list */}
-        <div className="w-full sm:flex sm:flex-wrap sm:justify-around">
-          {/* Cards */}
-          {statesArr?.attractionArr?.map((attractionItem, idx) => (
-            <AttractionCard
-              mediaUrl={attractionItem?.mediaUrl}
-              title={attractionItem?.title}
-              key={idx}
-            />
-          ))}
-        </div>
+        <ListCards />
       </AttractionContainer>
+    </>
+  );
+}
+
+function ListCards() {
+  // States
+  const statesArr = useAttractionListStates();
+
+  return (
+    <>
+      {/* Card list */}
+      <div className="w-full sm:flex sm:flex-wrap sm:justify-around">
+        {/* Cards */}
+        {statesArr?.attractionArr?.map((attractionItem, idx) => (
+          <AttractionCard
+            mediaUrl={attractionItem?.mediaUrl}
+            title={attractionItem?.title}
+            key={idx}
+          />
+        ))}
+      </div>
     </>
   );
 }
