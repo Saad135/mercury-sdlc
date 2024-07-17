c6bfd6319dd455d91246a2bd6b1115c5eefa5c0b
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-17 04:14:38 -0400

    Start server components for fetching saved lists

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 
+import SavedEventsServer from "@/components/app/user-saved-server";
 import Providers from "@/components/root-providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-gray-100`}>
         <div className="relative flex h-screen flex-col">
-          <Providers>{children}</Providers>
+          <Providers>
+            <SavedEventsServer>{children}</SavedEventsServer>
+          </Providers>
         </div>
       </body>
     </html>
---
+++ src/components/app/user-saved-server.js
@@ -0,0 +1,3 @@
+export default function SavedEventsServer({ children }) {
+  return <>{children}</>;
+}
