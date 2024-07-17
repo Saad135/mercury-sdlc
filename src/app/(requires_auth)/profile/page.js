3940b811a2ed73fcb45a5d59e21ffed6c167d721
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-16 22:21:30 -0400

    Make two panels on profile page

--- src/app/(requires_auth)/profile/page.js
+++ src/app/(requires_auth)/profile/page.js
@@ -3,7 +3,22 @@ import Container from "@/components/container";
 export default function ProfilePage() {
   return (
     <>
-      <Container>Hi</Container>
+      <div className="flex w-full flex-wrap">
+        <Container
+          className={
+            "container mx-auto my-2 rounded-xl bg-white p-4 shadow sm:w-[48%]"
+          }
+        >
+          Hi
+        </Container>
+        <Container
+          className={
+            "container mx-auto my-2 rounded-xl bg-white p-4 shadow sm:w-[48%]"
+          }
+        >
+          Hi
+        </Container>
+      </div>
     </>
   );
 }
