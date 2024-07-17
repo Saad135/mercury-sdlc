b3504dce8ccf506fb4ff59fd55ce38d2d71e392b
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-16 20:54:04 -0400

    Add category tabs to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,55 @@
+"use client";
+import { useEffect } from "react";
+
+import Container from "@/components/container";
 import EventList from "@/components/events/event-list";
+import { CategoryTabs } from "@/components/home/home-components";
+import { useHomeStates } from "@/components/home/home-provider";
 import MainDiv from "@/components/main-div";
 
 export default function Home() {
   return (
-    <MainDiv>
-      {/* List of cards */}
-      <EventList listName={"Attractions"} />
-    </MainDiv>
+    <>
+      {/* Categories section */}
+      <HomeCategories />
+
+      <MainSection />
+    </>
+  );
+}
+
+function MainSection() {
+  return (
+    <>
+      {/* Main section */}
+      <MainDiv>
+        {/* List of cards */}
+        <EventList listName={"Attractions"} />
+      </MainDiv>
+    </>
+  );
+}
+
+function HomeCategories() {
+  const { categories, selectedCategory, setSelectedCategory } = useHomeStates();
+
+  useEffect(() => {
+    document.getElementById(`cname_${selectedCategory}`).scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }, [selectedCategory]);
+
+  return (
+    <>
+      <Container>
+        <CategoryTabs
+          categoryNames={categories}
+          selectedCategoryName={selectedCategory}
+          setSelectedCategoryName={setSelectedCategory}
+        />
+      </Container>
+    </>
   );
 }
