0fcbc7ca4a097a5cb64e761c0894d6f6b74ef79c
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-16 23:18:10 -0400

    Add master list view with dummy data

--- src/app/(requires_auth)/profile/page.js
+++ src/app/(requires_auth)/profile/page.js
@@ -1,16 +1,24 @@
 import Container from "@/components/container";
+import EventList from "@/components/events/event-list";
 
 export default function ProfilePage() {
   return (
     <>
-      <div className="flex w-full flex-wrap">
-        <Container
+      <div className="flex w-full flex-wrap-reverse">
+        {/* Left Panel */}
+        <PanelContentEvents
+          className={
+            "container mx-auto my-2 rounded-xl bg-white p-4 shadow sm:w-[48%]"
+          }
+        />
+        {/* <Container
           className={
             "container mx-auto my-2 rounded-xl bg-white p-4 shadow sm:w-[48%]"
           }
         >
-          Hi
-        </Container>
+        </Container> */}
+
+        {/* Right Panel */}
         <Container
           className={
             "container mx-auto my-2 rounded-xl bg-white p-4 shadow sm:w-[48%]"
@@ -19,6 +27,22 @@ export default function ProfilePage() {
           Hi
         </Container>
       </div>
+    </>
+  );
+}
+
+function PanelContentEvents({ className }) {
+  const testObj = { attractionArr: [1, 2, 3, 4] };
+
+  return (
+    <>
+      <EventList
+        containerClassName={className}
+        listName={"Master List"}
+        statesObj={testObj}
+        cardsClassName={"w-full"}
+        cardItemClassName={"mx-2 mb-4 mt-2 border text-center shadow-lg w-full"}
+      />
     </>
   );
 }
