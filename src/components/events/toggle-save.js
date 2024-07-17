c32bbdc87d4e21a12500ceb0a4b33d85862e949f
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-16 23:43:09 -0400

    Add icons and toggle save button

---
+++ src/components/events/toggle-save.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { CheckMark, PlusSign, Spinner } from "../ui/icons";
+
+export default function ToggleSelection({
+  showToggleDesc = false,
+  extraClassnames = "",
+  isActive,
+  clickCallback,
+}) {
+  // is Loading state
+  const [isLoading, setIsLoading] = useState();
+
+  const handleClick = async ({ clickCallback }) => {
+    setIsLoading(true);
+
+    await clickCallback();
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <div
+        onClick={() => handleClick({ clickCallback: clickCallback })}
+        className={`m-1 flex cursor-pointer justify-center rounded-full p-2 shadow ${
+          !isActive ? "bg-white text-primary" : "bg-primary text-white"
+        } ${extraClassnames}`}
+      >
+        {isLoading ? <Spinner /> : !isActive ? <PlusSign /> : <CheckMark />}
+        {showToggleDesc && <span>{!isActive ? "Add" : "Remove"}</span>}
+      </div>
+    </>
+  );
+}
---
+++ src/components/ui/icons.js
@@ -0,0 +1,65 @@
+export function Spinner({ className }) {
+  return (
+    <>
+      <svg
+        className={className ? className : "size-6"}
+        // width="24"
+        // height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+          opacity=".25"
+        />
+        <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="0.75s"
+            values="0 12 12;360 12 12"
+            repeatCount="indefinite"
+          />
+        </path>
+      </svg>
+    </>
+  );
+}
+
+export function CheckMark() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.5 12.75l6 6 9-13.5"
+        />
+      </svg>
+    </>
+  );
+}
+
+export function PlusSign({ className }) {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={className ? className : "size-6"}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+      </svg>
+    </>
+  );
+}
