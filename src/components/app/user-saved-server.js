be1fffe546622951fb0e293724e78fedc3435dfa
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-17 05:14:42 -0400

    Remove log statement

--- src/components/app/user-saved-server.js
+++ src/components/app/user-saved-server.js
@@ -33,7 +33,7 @@ export default async function SavedEventsServer({ children }) {
   // reformat to web app formmat
   combinedEventArr = combinedEventArr?.map((event) => reformatEventItem(event));
 
-  console.log(combinedEventArr);
+  // console.log(combinedEventArr);
 
   return (
     <>
