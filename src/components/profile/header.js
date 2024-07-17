805b1dd80879c050465fe2bcb68f94cf15ae6824
Saad Mahmud <shuvro.mahmud79@gmail.com>
2024-07-16 22:12:20 -0400

    Add sign out button without functionality

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,12 +1,30 @@
+"use client";
 import { HeaderBase } from "../home/home-header";
 import HomeLogo from "../home/home-logo";
+import { BaseButton } from "../sigin-button";
 
 export function ProfileHeader() {
   return (
     <>
       <HeaderBase>
         <HomeLogo />
+
+        <ProfileSignOut />
       </HeaderBase>
+    </>
+  );
+}
+
+function ProfileSignOut() {
+  return (
+    <>
+      <BaseButton
+        btnText={"Sign Out"}
+        className={
+          "rounded-full border border-primary bg-primary p-2 text-white shadow hover:bg-primary/90 hover:shadow-sm active:bg-white active:text-primary"
+        }
+        onClickHandler={() => console.log("Sign out")}
+      />
     </>
   );
 }
